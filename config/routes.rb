4752ec2aaaef165eab1a67a061576b46a1f04096
Alexander Romanov <ar15121958@gmail.com>
2017-08-28 00:52:59 +0300

    Create model Contact and DB

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'main' => 'pages#main'
   get 'about' => 'pages#about'
   get '/' => 'pages#main'
+  post '/' => 'contacts#create'
+  resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
