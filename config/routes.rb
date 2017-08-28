08295cd98993d4f1ca0f759881981b09025d1084
Alexander Romanov <ar15121958@gmail.com>
2017-08-28 22:11:56 +0300

    Create Comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   get '/' => 'pages#main'
   post '/' => 'contacts#create'
+
+  resources :contacts do
+	   resources :comments
+  end
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827212427) do
+ActiveRecord::Schema.define(version: 20170828185819) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.integer "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_comments_on_contact_id"
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
