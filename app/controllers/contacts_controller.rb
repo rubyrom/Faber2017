37624cf3d112c6e6b250d4c2f1e518c3a197b7bf
Alexander Romanov <ar15121958@gmail.com>
2017-08-28 17:11:44 +0300

    Add contacts#destroy

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,6 +20,12 @@ class ContactsController < ApplicationController
     @contact=Contact.find(params[:id])
   end
 
+  def destroy
+  	@contact=Contact.find(params[:id])
+  	@contact.destroy
+  	  redirect_to contacts_path
+  end
+
 private
   def contact_params
     params.require(:contact).permit(:name, :phone, :email, :message)
