001e5487d1f2ca3e066ffb7f73f9c22e3132a5b4
Alexander Romanov <ar15121958@gmail.com>
2017-09-03 00:32:39 +0300

    Add comments_controller and forms for  comments in show.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+	  @contact=Contact.find(params[:contact_id])
+    @contact.comments.create(comment_params)
+    redirect_to contact_path(@contact)
+  end
+
+  private
+  def comment_params
+  	params.require(:comment).permit(:author, :body)
+  end
+
 end
