dbc6b5a84c7d335c856e2f847daccb6b55316f3e
Alexander Romanov <ar15121958@gmail.com>
2017-09-06 01:24:48 +0300

    Delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to contact_path(@contact)
   end
 
+  def destroy
+    @comment=Comment.find(params[:id])
+    @comment.destroy
+      redirect_to contacts_path
+  end
+
+
   private
   def comment_params
   	params.require(:comment).permit(:author, :body)
