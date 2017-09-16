bb4eeaa8da01eacecc210c56e84724c45fd849a6
Alexander Romanov <ar15121958@gmail.com>
2017-09-16 23:20:48 +0300

    New form for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,9 @@
 class ContactsController < ApplicationController
 
   layout "comments"
+  skip_before_action  :verify_authenticity_token
 
-before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def new
   end
